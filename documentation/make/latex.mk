bb8eb9b554dc2ef0da9e746391eef8f5594b834a
mikehobson <enquiries@metoffice.gov.uk>
2016-04-19 07:47:12 +0000

    ticket:553 for Matthew Hambley: Correctly build the data-model documentation.
    git-svn-id: https://metomi/svn/lfric.xm/Dynamo/trunk@5973 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- documentation/make/latex.mk
+++ documentation/make/latex.mk
@@ -10,13 +10,17 @@ BIBTEX   ?= bibtex
 INKSCAPE ?= inkscape
 EPSTOPDF ?= epstopdf
 
-export TEXINPUTS = build:../tex:
+export TEXINPUTS = build:$(DYNAMO_COMMON_FIGURES):../tex:/opt/ukmo/TeX/tex/latex/brand:
 export BIBINPUTS = ../bibliography:
 export BSTINPUTS = ../bibliography:
 
 DOCUMENT  = $(basename $(wildcard *.latex))
-FIGURES   = $(patsubst figures/%.svg,build/%.pdf,$(wildcard figures/*.svg)) \
-            $(patsubst figures/%.eps,build/%.pdf,$(wildcard figures/*.eps))
+
+# Locate all the "includegraphics" commands in the document and extract the
+# figure they include.
+FIGURES = $(shell grep -o -e '\\includegraphics[^{]*{[^}]*}' <$(DOCUMENT).latex | sed -n 's/.*{\(.*\)}/\1/p')
+FIGURES := $(patsubst %,build/%.pdf,$(FIGURES))
+
 BIBLIOGRAPHY = $(wildcard bibliography/*.bib)
 
 ifneq (x$(BIBLIOGRAPHY),x)
@@ -51,6 +55,10 @@ build/%.pdf: figures/%.svg | build
 build/%.pdf: figures/%.eps | build
 	@echo Rendering $<
 	$(Q)$(EPSTOPDF) --outfile=$@ $<
+
+build/%.pdf: $(DYNAMO_COMMON_FIGURES)/%.svg | build
+	@echo Rendering $<
+	$(Q)$(INKSCAPE) -z -f $< -A $@
 
 build:
 	@echo Creating $@

5affbf533e98305a1ca4456aa57e5967d30f18c4
ivakavcic <enquiries@metoffice.gov.uk>
2019-10-29 13:37:40 +0000

    ticket:1941 for Tom Bendall: Remove psykal lite code for initial_sample_rho
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@20839 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- gungho/source/kernel/initial_rho_sample_kernel_mod.F90
+++ gungho/source/kernel/initial_rho_sample_kernel_mod.F90
@@ -10,100 +10,110 @@
 
 module initial_rho_sample_kernel_mod
 
-    use argument_mod,         only : arg_type, func_type, &
-                                     GH_FIELD, GH_REAL,   &
-                                     GH_READ, GH_WRITE,   &
-                                     ANY_SPACE_9, CELLS
-    use constants_mod,        only : r_def
-    use fs_continuity_mod,    only : W3
-    use idealised_config_mod, only : test
-    use kernel_mod,           only : kernel_type
+  use argument_mod,         only : arg_type, func_type, &
+                                   GH_FIELD, GH_REAL,   &
+                                   GH_READ, GH_WRITE,   &
+                                   ANY_SPACE_9, CELLS,  &
+                                   GH_BASIS,            &
+                                   GH_DIFF_BASIS,       &
+                                   GH_EVALUATOR
+  use constants_mod,        only : r_def, i_def
+  use fs_continuity_mod,    only : W3
+  use idealised_config_mod, only : test
+  use kernel_mod,           only : kernel_type
 
-    implicit none
+  implicit none
 
-    !-------------------------------------------------------------------------
-    ! Public types
-    !-------------------------------------------------------------------------
-    !> The type declaration for the kernel. Contains the metadata needed by
-    !> the Psy layer.
-    !>
-    type, public, extends(kernel_type) :: initial_rho_sample_kernel_type
-        private
-        type(arg_type) :: meta_args(3) = (/                                 &
-            arg_type(GH_FIELD,   GH_WRITE,  W3),                            &
-            arg_type(GH_FIELD*3, GH_READ, ANY_SPACE_9),                     &
-            arg_type(GH_REAL,    GH_READ)                                   &
-            /)
-        integer :: iterates_over = CELLS
-    contains
-        procedure, nopass ::initial_rho_sample_code
-    end type
+  private
 
-    !-------------------------------------------------------------------------
-    ! Contained functions/subroutines
-    !-------------------------------------------------------------------------
-    public initial_rho_sample_code
+  !> The type declaration for the kernel. Contains the metadata needed by
+  !> the PSy layer.
+  !>
+  type, public, extends(kernel_type) :: initial_rho_sample_kernel_type
+    private
+    type(arg_type) :: meta_args(3) = (/             &
+        arg_type(GH_FIELD,   GH_WRITE,  W3),        &
+        arg_type(GH_FIELD*3, GH_READ, ANY_SPACE_9), &
+        arg_type(GH_REAL,    GH_READ)               &
+        /)
+    type(func_type) :: meta_funcs(1) = (/           &
+         func_type(ANY_SPACE_9, GH_BASIS)           &
+        /)
+    integer :: iterates_over = CELLS
+    integer :: gh_shape = GH_EVALUATOR
+  contains
+    procedure, nopass :: initial_rho_sample_kernel_code
+  end type
+
+  !-------------------------------------------------------------------------
+  ! Contained functions/subroutines
+  !-------------------------------------------------------------------------
+  public initial_rho_sample_kernel_code
 
 contains
 
-    !> @brief Computes the initial rho field
-    !! @param[in] nlayers Number of layers
-    !! @param[in] ndf_w3 Number of degrees of freedom per cell
-    !! @param[in] undf_w3 Total number of degrees of freedom
-    !! @param[in] map_w3 Dofmap for the cell at the base of the column
-    !! @param[inout] rho Density
-    !! @param[in] ndf_chi Number of degrees of freedom per cell for chi
-    !! @param[in] undf_chi Number of degrees of freedom for chi
-    !! @param[in] map_chi Dofmap for the cell at the base of the column for chi
-    !! @param[in] chi_basis Basis functions evaluated at gaussian quadrature points
-    !! @param[inout] chi_1 X component of the chi coordinate field
-    !! @param[inout] chi_2 Y component of the chi coordinate field
-    !! @param[inout] chi_3 Z component of the chi coordinate field
-    !! @param[in] time Current time of the model run
-    subroutine initial_rho_sample_code(nlayers, ndf_w3, undf_w3, map_w3, rho, &
-        ndf_chi, undf_chi, map_chi, chi_basis, chi_1, chi_2, chi_3, time)
+  !> @details Assigns initial rho field with analytic profile from analytic_density_profiles_mod
+  !! @param[in] nlayers Number of layers
+  !! @param[out] rho Density
+  !! @param[in] chi_1 X component of the chi coordinate field
+  !! @param[in] chi_2 Y component of the chi coordinate field
+  !! @param[in] chi_3 Z component of the chi coordinate field
+  !! @param[in] time Current time of the model run
+  !! @param[in] ndf_w3 Number of degrees of freedom per cell for rho
+  !! @param[in] undf_w3 Total number of degrees of freedom for rho
+  !! @param[in] map_w3 Dofmap for the cell at the base of the column for rho
+  !! @param[in] ndf_chi Number of degrees of freedom per cell for chi
+  !! @param[in] undf_chi Number of degrees of freedom for chi
+  !! @param[in] map_chi Dofmap for the cell at the base of the column for chi
+  !! @param[in] chi_basis Basis functions evaluated at gaussian quadrature points
+  subroutine initial_rho_sample_kernel_code(nlayers,                    &
+                                            rho,                        &
+                                            chi_1, chi_2, chi_3,        &
+                                            time,                       &
+                                            ndf_w3, undf_w3, map_w3,    &
+                                            ndf_chi, undf_chi, map_chi, &
+                                            chi_basis)
 
-        use analytic_density_profiles_mod, only : analytic_density
+    use analytic_density_profiles_mod, only : analytic_density
 
-        implicit none
+    implicit none
 
-        !Arguments
-        integer,                                        intent(in)  :: nlayers, ndf_w3, ndf_chi, undf_w3, undf_chi
-        integer, dimension(ndf_w3),                     intent(in)  :: map_w3
-        integer, dimension(ndf_chi),                    intent(in)  :: map_chi
-        real(kind=r_def), dimension(undf_w3),           intent(out) :: rho
-        real(kind=r_def), dimension(undf_chi),          intent(in)  :: chi_1, chi_2, chi_3
-        real(kind=r_def), dimension(1,ndf_chi, ndf_w3), intent(in)  :: chi_basis
-        real(kind=r_def),                               intent(in)  :: time
-        !Internal variables
-        integer               :: df1, df, k
+    ! Arguments
+    integer(kind=i_def),                            intent(in)  :: nlayers
+    integer(kind=i_def),                            intent(in)  :: ndf_w3, ndf_chi, undf_w3, undf_chi
+    integer(kind=i_def), dimension(ndf_w3),         intent(in)  :: map_w3
+    integer(kind=i_def), dimension(ndf_chi),        intent(in)  :: map_chi
+    real(kind=r_def), dimension(undf_w3),           intent(out) :: rho
+    real(kind=r_def), dimension(undf_chi),          intent(in)  :: chi_1, chi_2, chi_3
+    real(kind=r_def), dimension(1,ndf_chi, ndf_w3), intent(in)  :: chi_basis
+    real(kind=r_def),                               intent(in)  :: time
+    ! Internal variables
+    integer(kind=i_def)                                         :: df1, df, k
 
-        real(kind=r_def), dimension(ndf_chi)         :: chi_1_e, chi_2_e, chi_3_e
-        real(kind=r_def)                             :: x(3)
+    real(kind=r_def), dimension(ndf_chi)                        :: chi_1_e, chi_2_e, chi_3_e
+    real(kind=r_def)                                            :: x(3)
 
-        ! compute the RHS & LHS integrated over one cell and solve
-        do k = 0, nlayers-1
-            do df1 = 1, ndf_chi
-                chi_1_e(df1) = chi_1( map_chi(df1) + k)
-                chi_2_e(df1) = chi_2( map_chi(df1) + k)
-                chi_3_e(df1) = chi_3( map_chi(df1) + k)
-            end do
+    ! Compute the RHS & LHS integrated over one cell and solve
+    do k = 0, nlayers-1
+      do df1 = 1, ndf_chi
+        chi_1_e(df1) = chi_1( map_chi(df1) + k)
+        chi_2_e(df1) = chi_2( map_chi(df1) + k)
+        chi_3_e(df1) = chi_3( map_chi(df1) + k)
+      end do
 
-
-            do df = 1, ndf_w3
-              x(:) = 0.0_r_def
-              do df1 = 1, ndf_chi
-                x(1) = x(1) + chi_1_e(df1)*chi_basis(1,df1,df)
-                x(2) = x(2) + chi_2_e(df1)*chi_basis(1,df1,df)
-                x(3) = x(3) + chi_3_e(df1)*chi_basis(1,df1,df)
-              end do
-
-              rho(map_w3(df) + k) = analytic_density(x, test, time)
-
-            end do
+      do df = 1, ndf_w3
+        x(:) = 0.0_r_def
+        do df1 = 1, ndf_chi
+          x(1) = x(1) + chi_1_e(df1)*chi_basis(1,df1,df)
+          x(2) = x(2) + chi_2_e(df1)*chi_basis(1,df1,df)
+          x(3) = x(3) + chi_3_e(df1)*chi_basis(1,df1,df)
         end do
 
+        rho(map_w3(df) + k) = analytic_density(x, test, time)
 
-    end subroutine initial_rho_sample_code
+      end do
+    end do
+
+  end subroutine initial_rho_sample_kernel_code
 
 end module initial_rho_sample_kernel_mod

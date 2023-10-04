ticket:4062 for Declan Healy - Fix the units for plot rce
git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@45685 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

lfric_atm/rose-stem/app/plot/bin/plot_rce.py
@@ -56,8 +56,8 @@ def do_plot(datapath, plotfield, plotpath='.', plotlevel=0):
        lfric.convert_units('mm h-1')
 
     # Get the x and y co-ordinates
-    x_coord = (np.around(lfric.coord(x_coord_name).points, decimals=5))*10.0
-    y_coord = (np.around(lfric.coord(y_coord_name).points, decimals=5))*10.0
+    x_coord = (np.around(lfric.coord(x_coord_name).points, decimals=5))/1000.0
+    y_coord = (np.around(lfric.coord(y_coord_name).points, decimals=5))/1000.0
     t_coord = (np.around(lfric.coord('time').points, decimals=2))/3600.0
 
     # Save the min and max of the data